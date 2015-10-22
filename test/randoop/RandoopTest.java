565c48c9aa2525be6cf578a925de5d0ea2346c81
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-10-22 00:42:41 -0400

    new tests

--- test/randoop/RandoopTest.java
+++ test/randoop/RandoopTest.java
@@ -22,27 +22,6 @@ public class RandoopTest extends TestCase {
     result.addTest(new TestSuite(RandoopTest1.class));
     result.addTest(new TestSuite(RandoopTest2.class));
     result.addTest(new TestSuite(RandoopTest3.class));
-    result.addTest(new TestSuite(RandoopTest4.class));
-    result.addTest(new TestSuite(RandoopTest5.class));
-    result.addTest(new TestSuite(RandoopTest6.class));
-    result.addTest(new TestSuite(RandoopTest7.class));
-    result.addTest(new TestSuite(RandoopTest8.class));
-    result.addTest(new TestSuite(RandoopTest9.class));
-    result.addTest(new TestSuite(RandoopTest10.class));
-    result.addTest(new TestSuite(RandoopTest11.class));
-    result.addTest(new TestSuite(RandoopTest12.class));
-    result.addTest(new TestSuite(RandoopTest13.class));
-    result.addTest(new TestSuite(RandoopTest14.class));
-    result.addTest(new TestSuite(RandoopTest15.class));
-    result.addTest(new TestSuite(RandoopTest16.class));
-    result.addTest(new TestSuite(RandoopTest17.class));
-    result.addTest(new TestSuite(RandoopTest18.class));
-    result.addTest(new TestSuite(RandoopTest19.class));
-    result.addTest(new TestSuite(RandoopTest20.class));
-    result.addTest(new TestSuite(RandoopTest21.class));
-    result.addTest(new TestSuite(RandoopTest22.class));
-    result.addTest(new TestSuite(RandoopTest23.class));
-    result.addTest(new TestSuite(RandoopTest24.class));
     result.addTest(new TestSuite(randoopFailures.RandoopTest_failure_1.class));
     return result;
   }
