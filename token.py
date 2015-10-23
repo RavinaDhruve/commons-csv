669b7aba47ac7f98804cb0d22a425a4039269a5a
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-10-23 13:44:42 -0400

    Test tokens

---
+++ token.py
@@ -0,0 +1,19 @@
+import re
+import sys
+
+allLines = open(sys.argv[1])
+print allLines
+
+#with open("aws.js") as file_trial:
+#   allLines = file_trial.readlines();
+
+for l in allLines:
+    m = re.search("(\s|\"|\'|^|\A)AK[A-Z0-9]{18}(\s|\"|\'|$|\z)", l);
+        if m:
+            print "access : %s" % m;
+                print l
+        #True
+        p = re.search("(\s|\"|'|^)[a-zA-Z0-9~@#$^*()_+=\/[\]{}|\\,.?:-]{40}(\s|\"|'|$|\z)",l);
+        if p:
+            print "secret : %s" % p;
+                print l;
