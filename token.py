a22380b231f2d8e0ad7656b2ebfb2f117cdae226
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-10-23 14:48:30 -0400

    test

--- token.py
+++ token.py
@@ -6,15 +6,15 @@ print allLines
 
 #with open("aws.js") as file_trial:
 #   allLines = file_trial.readlines();
-print "working please"
+print "working please now!!!!"
 
 for l in allLines:
     m = re.search("(\s|\"|\'|^|\A)AK[A-Z0-9]{18}(\s|\"|\'|$|\z)", l);
-        if m:
-            print "access : %s" % m;
-                print l
+    if m:
+        print "access : %s" % m;
+        print l
         #True
-        p = re.search("(\s|\"|'|^)[a-zA-Z0-9~@#$^*()_+=\/[\]{}|\\,.?:-]{40}(\s|\"|'|$|\z)",l);
-        if p:
-            print "secret : %s" % p;
-                print l;
+    p = re.search("(\s|\"|'|^)[a-zA-Z0-9~@#$^*()_+=\/[\]{}|\\,.?:-]{40}(\s|\"|'|$|\z)",l);
+    if p:
+        print "secret : %s" % p;
+        print l;
