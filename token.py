9cf0683d19c95738a26755aca26144c1e501f0d7
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-10-23 15:29:27 -0400

    test_token

--- token.py
+++ token.py
@@ -1,5 +1,6 @@
 import re
 import sys
+import os
 
 fileHandle = open(sys.argv[1])
 allLines = fileHandle.readlines();
@@ -10,8 +11,28 @@ for l in allLines:
     if m:
         print "access : %s" % m;
         print l
+        sys.stderr.write ('Access key ID found \n')
+        sys.exit(-1)
         #True
     p = re.search("(\s|\"|'|^)[a-zA-Z0-9~@#$^*()_+=\/[\]{}|\\,.?:-]{40}(\s|\"|'|$|\z)",l);
     if p:
         print "secret : %s" % p;
         print l;
+    r = re.search("(\s|\"|'|^)[a-zA-Z0-9]{64}(\s|\"|'|$|\z)",l);
+    if r:
+        print "token : %s" % r;
+        print l;
+
+
+'''
+import sys
+filenames = os.listdir(os.curdir)
+
+for filename in filenames:
+    if os.path.isfile(filename) and filename.endswith(".txt"):
+        sys.stderr.write ('File exists \n')
+        break
+else:
+    sys.stderr.write ('No database file found. Exiting program. \n')
+    sys.exit(-1)
+'''
