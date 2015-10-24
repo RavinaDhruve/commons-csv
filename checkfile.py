989031d498cd2852ecb74c7298e710e4d5571b60
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-10-23 21:02:53 -0400

    run

--- checkfile.py
+++ checkfile.py
@@ -1,10 +1,10 @@
 import os
 import sys
 filenames = os.listdir(sys.argv[1])
-filetype = 'credentials.csv'
+#filetype = 'credentials.csv'
 ext = '.pem'
 
 for filename in filenames:
-    if filetype in filename or ext in filename:
+    if ext in filename:
         sys.stderr.write ('File exists : %s\n'%filename)
         sys.exit(-1)
--- token.py
+++ token.py
@@ -10,20 +10,20 @@ allLines = fileHandle.readlines();
 for l in allLines:
     m = re.search("(\s|\"|\'|^|\A)AK[A-Z0-9]{18}(\s|\"|\'|$|\z)", l);
     if m:
-        print "access : %s" % m;
-        print l
+        #print "access : %s" % m;
+        #print l
         sys.stderr.write ('Access key ID found \n')
         sys.exit(-1)
     p = re.search("(\s|\"|'|^)[a-zA-Z0-9~@#$^*()_+=\/[\]{}|\\,.?:-]{40}(\s|\"|'|$|\z)",l);
     if p:
-        print "secret : %s" % p;
-        print l;
+        #print "secret : %s" % p;
+        #print l;
         sys.stderr.write ('Secret Access key found \n')
         sys.exit(-1)
     r = re.search("(\s|\"|'|^)[a-zA-Z0-9]{64}(\s|\"|'|$|\z)",l);
     if r:
-        print "token : %s" % r;
-        print l;
+        #print "token : %s" % r;
+        #print l;
         sys.stderr.write ('Digital ocean token \n')
         sys.exit(-1)
 
