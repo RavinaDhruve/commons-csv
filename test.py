a667d3b80d6305e039b78e6e839ceb69e5cc4f25
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-10-23 20:27:28 -0400

    try

--- test.py
+++ test.py
@@ -28,3 +28,7 @@ for line in fhandle:
             code = code + 1;
 
 print ("Comments:Code = %d:%d" % (count,code))
+ratio = count/code
+if(ratio > 1)
+    sys.stderr.write("Cannot exceed defined comment:code ratio")
+    sys.exit(-1)
